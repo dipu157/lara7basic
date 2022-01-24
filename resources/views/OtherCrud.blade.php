e40871aea7279fd4603d9c96bca3becc78a01036
dipu157 <dipu157@gmail.com>
2022-01-24 11:05:59 +0600

    other crud ADD module continue...

--- resources/views/OtherCrud.blade.php
+++ resources/views/OtherCrud.blade.php
@@ -1,98 +1,240 @@
 @extends('layout.app')
 
 @section('content')
-
-<div id="mainDiv" class="container">
-  <div class="row">
-    <div class="col-md-12 p-5">
-
-      <button id="adNewId" class="btn btn-primary my-3">Add New</button>
-
-      <table id="TableId" class="table table-striped table-bordered" cellspacing="0" width="100%">
-        <thead>
-          <tr>
-            <th class="th-sm">UserName</th>
-            <th class="th-sm">Gender</th>
-            <th class="th-sm">Speciality</th>
-            <th class="th-sm">DOB</th>
-            <th class="th-sm">Photo</th>
-            <th class="th-sm">Status</th>
-            <th class="th-sm">Image</th>
-            <th class="th-sm">Action</th>
-          </tr>
-        </thead>
-        <tbody id="othercrud_table">
-
-        </tbody>
-
-      </table>
-
+<div class="container" id="mainDiv">
+    <div class="row">
+        <div class="col-md-12 p-5">
+            <button class="btn btn-primary my-3" id="adNewId">
+                Add New
+            </button>
+            <table cellspacing="0" class="table table-striped table-bordered" id="TableId" width="100%">
+                <thead>
+                    <tr>
+                        <th class="th-sm">
+                            UserName
+                        </th>
+                        <th class="th-sm">
+                            Gender
+                        </th>
+                        <th class="th-sm">
+                            Speciality
+                        </th>
+                        <th class="th-sm">
+                            DOB
+                        </th>
+                        <th class="th-sm">
+                            Photo
+                        </th>
+                        <th class="th-sm">
+                            Status
+                        </th>
+                        <th class="th-sm">
+                            Image
+                        </th>
+                        <th class="th-sm">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody id="othercrud_table">
+                </tbody>
+            </table>
+        </div>
     </div>
-  </div>
 </div>
-
-<div id="loadDiv" class="container">
-  <div class="row">
-    <div class="col-md-12 text-center p-5">
-      <img class="loading-icon" src="{{asset('images/loader.gif')}}">
-
+<div class="container" id="loadDiv">
+    <div class="row">
+        <div class="col-md-12 text-center p-5">
+            <img class="loading-icon" src="{{asset('images/loader.gif')}}">
+            </img>
+        </div>
     </div>
-  </div>
 </div>
-
-<div id="errDiv" class="container d-none">
-  <div class="row">
-    <div class="col-md-12 p-5">
-      <h3>Data Not Found. Something went wrong</h3>
-
+<div class="container d-none" id="errDiv">
+    <div class="row">
+        <div class="col-md-12 p-5">
+            <h3>
+                Data Not Found. Something went wrong
+            </h3>
+        </div>
     </div>
-  </div>
 </div>
-
+<!-- Modal For Add  -->
+<div aria-hidden="true" aria-labelledby="exampleModalLabel" class="modal fade" id="addModal" role="dialog" tabindex="-1">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <!--Header-->
+            <div class="modal-header" style="background-color: #17A2B8;">
+                <p class="heading">
+                    Add New
+                </p>
+            </div>
+            <!--Body-->
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card text-primary bg-gray">
+                            <div class="card-body">
+                                <div class="form-group row" id="md-name">
+                                    <label class="col-sm-4 col-form-label text-md-right" for="name">
+                                        User Name
+                                    </label>
+                                    <div class="col-sm-8">
+                                        <select class="browser-default custom-select">
+                                            <option selected="">
+                                                Select UserName
+                                            </option>
+                                            <option value="1">
+                                                One
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group row" id="md-name">
+                                    <label class="col-sm-4 col-form-label text-md-right" for="gender">
+                                        Gender
+                                    </label>
+                                    <div class="col-sm-8">
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" id="gender1" name="gender" type="radio" value="M">
+                                                <label class="form-check-label" for="gender1">
+                                                    Male
+                                                </label>
+                                            </input>
+                                        </div>
+                                        <!-- Material inline 2 -->
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" id="gender2" name="gender" type="radio" value="F">
+                                                <label class="form-check-label" for="gender2">
+                                                    Female
+                                                </label>
+                                            </input>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group row" id="md-name">
+                                    <label class="col-sm-4 col-form-label text-md-right" for="speciality">
+                                        Speciality
+                                    </label>
+                                    <div class="col-sm-8">
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" id="speciality1" name="speciality" type="checkbox" value="PHP">
+                                                <label class="form-check-label" for="speciality1">
+                                                    PHP
+                                                </label>
+                                            </input>
+                                        </div>
+                                        <!-- Material inline 2 -->
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" id="speciality2" name="speciality" type="checkbox" value="MySql">
+                                                <label class="form-check-label" for="speciality2">
+                                                    MySql
+                                                </label>
+                                            </input>
+                                        </div>
+                                        <!-- Material inline 3 -->
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" id="speciality3" name="speciality" type="checkbox" value="Java">
+                                                <label class="form-check-label" for="speciality3">
+                                                    Java
+                                                </label>
+                                            </input>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" id="speciality4" name="speciality" type="checkbox" value="Oracle">
+                                                <label class="form-check-label" for="speciality4">
+                                                    Oracle
+                                                </label>
+                                            </input>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" id="speciality5" name="speciality" type="checkbox" value="Python">
+                                                <label class="form-check-label" for="speciality5">
+                                                    Python
+                                                </label>
+                                            </input>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-4 col-form-label text-md-right" for="dob">
+                                        DOB
+                                    </label>
+                                    <div class="col-sm-8">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Footer-->
+            <div class="modal-footer justify-content-center">
+                <a class="btn btn-danger waves-effect" data-dismiss="modal" type="button">
+                    Cancel
+                </a>
+                <button class="btn btn-primary" type="submit">
+                    Save
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- Modal For Add Image -->
-<div class="modal fade" id="photoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header p-3 text-center">
-        <h5 class="modal-title">Add Photo</h5>
-        <input id="ImgId" value="" type="text">
-      </div>
-
-      <div class="modal-body">
-        <input id="imgInput" type="file">
-        <img style="width: 100px; height: 100px;" src="" id="imgPreview">
-      </div>
-
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button id="photoAddBtn" type="button" class="btn btn-success">Upload</button>
-      </div>
+<div aria-hidden="true" aria-labelledby="exampleModalLabel" class="modal fade" id="photoModal" role="dialog" tabindex="-1">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header p-3 text-center">
+                <h5 class="modal-title">
+                    Add Photo
+                </h5>
+                <input id="ImgId" type="text" value="">
+                </input>
+            </div>
+            <div class="modal-body">
+                <input id="imgInput" type="file">
+                    <img id="imgPreview" src="" style="width: 100px; height: 100px;">
+                    </img>
+                </input>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" data-dismiss="modal" type="button">
+                    Close
+                </button>
+                <button class="btn btn-success" id="photoAddBtn" type="button">
+                    Upload
+                </button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
-
-
 <!-- Modal For Delete -->
-<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-
-      <div class="modal-body text-center">
-        <h6>Are You Sure about Delete ?</h6>
-        <input id="DeleteId" value="" type="hidden">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button id="DelConfirmBtn" type="button" class="btn btn-danger">Delete</button>
-      </div>
+<div aria-hidden="true" aria-labelledby="exampleModalLabel" class="modal fade" id="deleteModal" role="dialog" tabindex="-1">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body text-center">
+                <h6>
+                    Are You Sure about Delete ?
+                </h6>
+                <input id="DeleteId" type="hidden" value="">
+                </input>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" data-dismiss="modal" type="button">
+                    Close
+                </button>
+                <button class="btn btn-danger" id="DelConfirmBtn" type="button">
+                    Delete
+                </button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 @endsection
 
 @section('script')
 <script type="text/javascript">
-  getOtherCrudData();
+    getOtherCrudData();
 
   function getOtherCrudData() {
 
@@ -111,7 +253,7 @@
 
 
           $.each(dataJSON, function(i, item) {
-            var gender = dataJSON[i].gender == 'm' ? 'Male' : 'Female';
+            var gender = dataJSON[i].gender == 'M' ? 'Male' : 'Female';
             var status = dataJSON[i].status == 1 ? 'Active' : 'Inactive';
             $('<tr>').html(
               "<td>" + dataJSON[i].basic_crud['full_name'] + "</td>" +
@@ -194,6 +336,11 @@
       }).catch(function(error) {
         alert(error);
       })
+  });
+
+  //Add New Btn Clik
+  $('#adNewId').click(function() {
+    $('#addModal').modal('show');
   });
 
   //Confirm Delete
